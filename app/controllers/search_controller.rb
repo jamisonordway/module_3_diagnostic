5158ff1942536777a3c459fc68c2542b07027406
JamisonOrdway <jamisonordway@gmail.com>
2018-07-11 10:45:40 -0600

    Changes API call in StationSearchResult, WIP

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    @stations = StationSearchResult.new(params[:q])
+    @stations = StationSearchResult.new(params[:q]).stations_call
   end
 end
--- app/models/station_search_result.rb
+++ app/models/station_search_result.rb
@@ -4,12 +4,12 @@ class StationSearchResult
     @zip = zip
   end
 
-  def stations
+  def stations_call
     conn = Faraday.new(:url => "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=10&api_key=altfuel_key&format=JSON") do |faraday|
     faraday.headers["X-API-KEY"] = ENV['altfuel_key']
     faraday.adapter Faraday.default_adapter
     end 
-    response = JSON.parse(conn.get('/altfuel/v1/stations/zip?=80203'))
+    response = conn.get '/altfuel/v1/stations/zip?=80203' 
     data = JSON.parse(response.body, symbolize_names: true)
     data[:results].map do |fuel_station_data|
         Station.new(fuel_station_data)
